b9196902dc3867967cda30bd3def9c5aead79ac0
ohamszy <38721811+ohamszy@users.noreply.github.com>
2018-04-25 14:03:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+echo "<!DOCTYPE HTML>
+<html>
+<head>
+<title> stage1</title>
+<link rel='stylesheet' href='slack.css'>
+</head>
+<body>
+<div class='container'>
+<header>
+<ul>
+	<li><a href='#'>Home</a></li>
+	<li><a href='#'>GALLERY</a></li>
+	<li><a href='#'>ABOUT US</a></li></ul>
+</header>
+
+<br><br>
+<br>
+<br>
+<br>
+<br>
+
+
+<div class='main'>";
+
+$time=date("h:i:A", time());
+echo "CURRENT TIME: $time
+</div>
+
+<h5>
+THIS WEBSITE TELL US THE TIME IN ALL REGIONS
+
+</h5>
+</div>
+</body>
+</html>";
+?>
